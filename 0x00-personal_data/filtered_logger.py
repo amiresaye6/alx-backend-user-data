dd4eefcb00229540c4bb97964c0204bdde74cb8f
Amir Alsayed <amyralsyd367@gmail.com>
2024-07-04 19:53:53 +0300

    pycodestyle fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,14 +14,3 @@ def filter_datum(fields: list, redaction: str, message: str,
         message = re.sub(r'({}=)[^{}]+'.format(field, separator),
                          r'\1{}'.format(redaction), message)
     return message
-
-
-# # test
-# fields = ["password", "date_of_birth", 'email']
-# messages = [
-# "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
-# "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"
-#     ]
-
-# for message in messages:
-#     print(filter_datum(fields, 'xxx', message, ';'))
--- 0x00-personal_data/notes/note.py
+++ 0x00-personal_data/notes/note.py
@@ -4,6 +4,17 @@ notes and study module
 resources : 
     https://docs.python.org/3/library/logging.html
 
+
+# DEBUG: Detailed information, typically of interest only when diagnosing problems.
+
+# INFO: Confirmation that things are working as expected.
+
+# WARNING: An indication that something unexpected happened, or indicative of some problem in the near future (e.g. ‘disk space low’). The software is still working as expected.
+
+# ERROR: Due to a more serious problem, the software has not been able to perform some function.
+
+# CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
+
 """
 # myapp.py
 import logging
@@ -19,3 +30,16 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+
+# # test
+# fields = ["password", "date_of_birth", 'email']
+# messages = [
+# "name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;",
+# "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"
+#     ]
+
+# for message in messages:
+#     print(filter_datum(fields, 'xxx', message, ';'))
