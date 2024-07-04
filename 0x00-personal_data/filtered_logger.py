59ea69741933a4442581def3d52066e12276f031
Amir Alsayed <amyralsyd367@gmail.com>
2024-07-04 20:00:36 +0300

    refactoring code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,14 +3,26 @@
 filter datum module using re and logging
 """
 import re
+from typing import List
 
 
-def filter_datum(fields: list, redaction: str, message: str,
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """
-    Returns the log message obfuscated
+    Returns the log message with specified fields obfuscated.
+
+    Args:
+        fields (List[str]): A list of strings representing the fields to be
+        obfuscated.
+        redaction (str): The string used for obfuscation.
+        message (str): The log message to be filtered.
+        separator (str): The separator used to separate the fields in the log
+        message.
+
+    Returns:
+        str: The filtered log message with specified fields obfuscated.
     """
     for field in fields:
-        message = re.sub(r'({}=)[^{}]+'.format(field, separator),
-                         r'\1{}'.format(redaction), message)
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
