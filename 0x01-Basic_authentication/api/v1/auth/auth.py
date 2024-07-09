1771c178268a2053f60d7006d9d1a19aeae19d47
Amir Alsayed <amyralsyd367@gmail.com>
2024-07-09 21:34:23 +0300

    require_auth test 1

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,7 +5,7 @@ authentication.
 """
 from flask import request
 from typing import List
-from typing import List, TypeVar
+from typing import TypeVar
 
 
 class Auth:
@@ -24,7 +24,12 @@ class Auth:
         Returns:
             bool: True if authentication is required, False otherwise.
         """
-        return False
+        if (not path or not excluded_paths or len(excluded_paths) == 0):
+            return True
+
+        for epath in excluded_paths:
+            if epath == path or epath[:-1] == path:
+                return False
 
     def authorization_header(self, request=None) -> str:
         """
